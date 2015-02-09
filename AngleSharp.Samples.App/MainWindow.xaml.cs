3de613a052563663f1bf8de5ccf227161ba4cc82
Florian Rappl <rappl.florian@gmail.com>
2015-02-09 21:34:47 +0100

    Added existing sample code

---
+++ AngleSharp.Samples.App/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Samples
+{
+    using FirstFloor.ModernUI.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AngleSharp.Samples.App/Pages/Main.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Samples.Pages
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
