b758c048ef94adbc3b50db977bf84494c8e6ea17
Florian Rappl <rappl.florian@gmail.com>
2015-04-16 11:35:45 +0200

    Improved plot display

--- AngleSharp.Samples.App/ViewModels/ProfilerViewModel.cs
+++ AngleSharp.Samples.App/ViewModels/ProfilerViewModel.cs
@@ -2,21 +2,22 @@
 {
     using AngleSharp.Events;
     using OxyPlot;
+    using OxyPlot.Axes;
+    using OxyPlot.Series;
     using System;
-    using System.Collections.ObjectModel;
     using System.Diagnostics;
 
-    public class ProfilerViewModel : BaseViewModel
+    public class ProfilerViewModel : BaseViewModel, IEventViewModel
     {
-        readonly Collection<Item> _items;
         readonly IEventAggregator _events;
         readonly Stopwatch _time;
+        readonly PlotModel _model;
 
         public ProfilerViewModel(IEventAggregator events)
         {
             _time = new Stopwatch();
-            _items = new Collection<Item>();
             _events = events;
+            _model = CreateModel();
             Register<CssParseStartEvent>(m =>
             {
                 var start = _time.Elapsed;
@@ -34,9 +35,30 @@
             });
         }
 
+        static PlotModel CreateModel()
+        {
+            var model = new PlotModel { LegendPlacement = LegendPlacement.Outside };
+            var valueAxis = new LinearAxis { Position = AxisPosition.Bottom, MinimumPadding = 0.1, MaximumPadding = 0.1 };
+            var categoryAxis = new CategoryAxis { Position = AxisPosition.Left };
+            var series = new IntervalBarSeries();
+            model.Axes.Add(categoryAxis);
+            model.Axes.Add(valueAxis);
+            model.Series.Add(series);
+            return model;
+        }
+
         void AddItem(String label, OxyColor color, TimeSpan start, TimeSpan end)
         {
-            _items.Add(new Item { Label = label, Color = color, Start = start, End = end });
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                var series = _model.Series[0] as IntervalBarSeries;
+                var category = _model.Axes[0] as CategoryAxis;
+                category.Labels.Add((category.Labels.Count + 1).ToString());
+                var begin = start.TotalMilliseconds;
+                var final = end.TotalMilliseconds;
+                series.Items.Add(new IntervalBarItem(begin, final, label) { Color = color });
+                _model.InvalidatePlot(true);
+            });
         }
 
         void Register<T>(Action<T> listener)
@@ -45,47 +67,16 @@
             _events.Subscribe(subscriber);
         }
 
+        public PlotModel PlotData
+        {
+            get { return _model; }
+        }
+
         public void Reset()
         {
-            _items.Clear();
+            (_model.Series[0] as IntervalBarSeries).Items.Clear();
+            (_model.Axes[0] as CategoryAxis).Labels.Clear();
             _time.Restart();
-        }
-
-        public Collection<Item> Items
-        {
-            get { return _items; }
-        }
-
-        public class Item
-        {
-            public String Label
-            {
-                get;
-                set;
-            }
-
-            public TimeSpan Start
-            {
-                get;
-                set;
-            }
-
-            public TimeSpan End
-            {
-                get;
-                set;
-            }
-
-            public Double Value
-            {
-                get { return End.TotalMilliseconds - Start.TotalMilliseconds; }
-            }
-
-            public OxyColor Color
-            {
-                get;
-                set;
-            }
         }
     }
 }
