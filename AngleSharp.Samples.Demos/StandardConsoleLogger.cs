b91d6f861fcf67c5d494b6d3ff87eeae12046d08
Florian Rappl <rappl.florian@gmail.com>
2016-07-17 11:39:01 +0200

    Updated to AngleSharp v0.9.7

---
+++ AngleSharp.Samples.Demos/StandardConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿namespace AngleSharp.Samples.Demos
+{
+    using AngleSharp.Scripting.JavaScript.Services;
+    using System;
+    using System.Linq;
+
+    sealed class StandardConsoleLogger : IConsoleLogger
+    {
+        public void Log(Object[] values)
+        {
+            var elements = values.Select(m => (m ?? String.Empty).ToString());
+            var content = String.Join(", ", elements);
+            Console.WriteLine(content);
+        }
+    }
+}
